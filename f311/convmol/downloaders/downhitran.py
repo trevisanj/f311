d55ba0dfc598d7a4295ac5b5b02dbe0ae5a76b6a
Julio Trevisan <juliotrevisan@gmail.com>
2017-10-04 09:07:01 -0300

    hitran obs

--- f311/convmol/downloaders/downhitran.py
+++ f311/convmol/downloaders/downhitran.py
@@ -1,5 +1,18 @@
 """
-HITRAN downloader
+HITRAN Database [Gordon2016] downloader
+
+References:
+
+[Gordon2016] I.E. Gordon, L.S. Rothman, C. Hill, R.V. Kochanov, Y. Tan, P.F. Bernath, M. Birk,
+    V. Boudon, A. Campargue, K.V. Chance, B.J. Drouin, J.-M. Flaud, R.R. Gamache, J.T. Hodges,
+    D. Jacquemart, V.I. Perevalov, A. Perrin, K.P. Shine, M.-A.H. Smith, J. Tennyson, G.C. Toon,
+    H. Tran, V.G. Tyuterev, A. Barbe, A.G. Császár, V.M. Devi, T. Furtenbacher, J.J. Harrison,
+    J.-M. Hartmann, A. Jolly, T.J. Johnson, T. Karman, I. Kleiner, A.A. Kyuberis, J. Loos,
+    O.M. Lyulin, S.T. Massie, S.N. Mikhailenko, N. Moazzen-Ahmadi, H.S.P. Müller, O.V. Naumenko,
+    A.V. Nikitin, O.L. Polyansky, M. Rey, M. Rotger, S.W. Sharpe, K. Sung, E. Starikova,
+    S.A. Tashkun, J. Vander Auwera, G. Wagner, J. Wilzewski, P. Wcisło, S. Yu, E.J. Zak,
+    The HITRAN2016 Molecular Spectroscopic Database, J. Quant. Spectrosc. Radiat. Transf. (2017).
+    doi:10.1016/j.jqsrt.2017.06.038.
 """
 
 from robobrowser import RoboBrowser
--- f311/convmol/scripts/hitran-scraper.py
+++ f311/convmol/scripts/hitran-scraper.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python
 
 """
-Retrieves molecular lines from the HITRAN database
+Retrieves molecular lines from the HITRAN database [Gordon2016]
 
 This script uses web scraping and the HAPI to save locally molecular lines from the HITRAN database.
 
 While the HAPI provides the downloading facility, web scraping is used to get the lists of molecules
 and isotopologues from the HITRAN webpages and get the IDs required to run the HAPI query.
 
-The script is typically invoked several times, each time with an additional argument
+The script is typically invoked several times, each time with an additional argument.
+
+References:
+
+[Gordon2016] I.E. Gordon, L.S. Rothman, C. Hill, R.V. Kochanov, Y. Tan, P.F. Bernath, M. Birk,
+    V. Boudon, A. Campargue, K.V. Chance, B.J. Drouin, J.-M. Flaud, R.R. Gamache, J.T. Hodges,
+    D. Jacquemart, V.I. Perevalov, A. Perrin, K.P. Shine, M.-A.H. Smith, J. Tennyson, G.C. Toon,
+    H. Tran, V.G. Tyuterev, A. Barbe, A.G. Császár, V.M. Devi, T. Furtenbacher, J.J. Harrison,
+    J.-M. Hartmann, A. Jolly, T.J. Johnson, T. Karman, I. Kleiner, A.A. Kyuberis, J. Loos,
+    O.M. Lyulin, S.T. Massie, S.N. Mikhailenko, N. Moazzen-Ahmadi, H.S.P. Müller, O.V. Naumenko,
+    A.V. Nikitin, O.L. Polyansky, M. Rey, M. Rotger, S.W. Sharpe, K. Sung, E. Starikova,
+    S.A. Tashkun, J. Vander Auwera, G. Wagner, J. Wilzewski, P. Wcisło, S. Yu, E.J. Zak,
+    The HITRAN2016 Molecular Spectroscopic Database, J. Quant. Spectrosc. Radiat. Transf. (2017).
+    doi:10.1016/j.jqsrt.2017.06.038.
 """
 
 import argparse
