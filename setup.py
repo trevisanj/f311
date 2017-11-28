16255e4457996acbed99582938ca19f45583e359
trevisanj <juliotrevisan@gmail.com>
2017-11-28 17:19:08 -0200

    release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='f311',
     packages=find_packages(),
     include_package_data=True,
-    version='0.17.11.27.0',
+    version='0.17.11.28.0',
     license='GNU GPLv3',
     platforms='any',
     description='Astronomy-related API, command-line tools, and windowed applications',
@@ -35,7 +35,7 @@ setup(
     url='https://github.com/trevisanj/f311',
     keywords= ['astronomy', "fits", "spectroscopy", "spectral synthesis", "photometry",
                "honl-london", "nist", "hitran", "multiplicity"],
-    install_requires=["a99>=0.17.12.08.3"],
+    install_requires=["a99>=0.17.12.08.4"],
     scripts=scripts
 )
 
